7872454505365241d05477ba198df9f92b58f557
Tiredbug <im.basoro@gmail.com>
2015-02-04 09:51:40 +0700

    Update cpt.php

--- cpt.php
+++ cpt.php
@@ -1,17 +1,17 @@
 <?php
 
-class TiredbugPostType {
+class CptPostType {
 
 	public function __construct(){
-		add_theme_support('post-thumbnails', array('tiredbug'));
+		add_theme_support('post-thumbnails', array('cpt'));
 		add_action('add_meta_boxes', array($this, 'add_meta_box'));
 		add_action('save_post', array($this, 'save'));
     
-		register_post_type('tiredbug',
+		register_post_type('cpt',
 			array(
 				'labels'			=> array(
-					'name'			=> __('Tiredbugs'),
-					'singular_name'		=> __('Tiredbug')
+					'name'			=> __('Cpts'),
+					'singular_name'		=> __('Cpt')
 		  		),
 	  			'public'			=> true,
 		  		'has_archive'			=> true,
@@ -21,19 +21,19 @@ class TiredbugPostType {
   	}
 
   	public function add_meta_box($post_type){
- 		$post_types = array("tiredbug");
+ 		$post_types = array("cpt");
 	  	if(in_array($post_type, $post_types)){
-		  	add_meta_box("tiredbug-data", "Tiredbug Data", array($this, "data_meta_box_content"), "tiredbug", "normal", "core");
+		  	add_meta_box("cpt-data", "Cpt Data", array($this, "data_meta_box_content"), "cpt", "normal", "core");
 	  	}
   	}
 
   	public function save($post_id){
-	  	if(!isset($_POST["tiredbug_nonce"]))
+	  	if(!isset($_POST["cpt_nonce"]))
 		  	return $post_id;
 
-	  	$nonce = $_POST["tiredbug_nonce"];
+	  	$nonce = $_POST["cpt_nonce"];
 
-	  	if(!wp_verify_nonce($nonce, "tiredbug"))
+	  	if(!wp_verify_nonce($nonce, "cpt"))
 		  	return $post_id;
 
   		if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
@@ -43,8 +43,8 @@ class TiredbugPostType {
 	  		return $post_id;
 
   		$fields = array(
-	  		'name'		=> sanitize_text_field($_POST["tiredbug_name"]),
-		  	'address'	=> sanitize_text_field($_POST["tiredbug_address"])
+	  		'name'		=> sanitize_text_field($_POST["cpt_name"]),
+		  	'address'	=> sanitize_text_field($_POST["cpt_address"])
   		);
 
 	  	foreach($fields as $k=>$v){
@@ -53,19 +53,19 @@ class TiredbugPostType {
   	}
 
   	public function data_meta_box_content($post){
-	  	wp_nonce_field("tiredbug", "tiredbug_nonce");
+	  	wp_nonce_field("cpt", "cpt_nonce");
 
 	  	$name = get_post_meta($post->ID, "name", true);
 	  	$address = get_post_meta($post->ID, "address", true);
   	?>
 	  	<table class="form-table">
 		  	<tr>
-			  	<th><label for="tiredbug_name"><?php _e("Name", "tiredbug"); ?></label></th>
-			  	<td><input type="text" name="tiredbug_name" id="tiredbug_name" value="<?php echo $name; ?>" class="regular-text"></td>
+			  	<th><label for="cpt_name"><?php _e("Name", "cpt"); ?></label></th>
+			  	<td><input type="text" name="cpt_name" id="cpt_name" value="<?php echo $name; ?>" class="regular-text"></td>
 		  	</tr>
 		  	<tr>
-			  	<th><label for="tiredbug_address"><?php _e("Address", "tiredbug"); ?></label></th>
-			  	<td><textarea name="tiredbug_address" id="tiredbug_address"><?php echo $address; ?></textarea></td>
+			  	<th><label for="cpt_address"><?php _e("Address", "cpt"); ?></label></th>
+			  	<td><textarea name="cpt_address" id="cpt_address"><?php echo $address; ?></textarea></td>
 		  	</tr>
 	  	</table>
   	<?php
